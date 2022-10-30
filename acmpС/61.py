17025cd87560db9117b8ed90bd83244616c08b59
renroy34 <115404396+renroy34@users.noreply.github.com>
2022-10-30 14:49:46 +0300

    Add files via upload

---
+++ acmpС/61.py
@@ -0,0 +1,18 @@
+a, b= input().split()
+a = int(a)
+b = int(b)
+c, d= input().split()
+c = int(c)
+d = int(d)
+m, n= input().split()
+m = int(m)
+n = int(n)
+k, l= input().split()
+k = int(k)
+l = int(l)
+if a + c + m + k > b + d + n + l:
+    print(1)
+elif a + c + m + k < b + d + n + l:
+    print(2)
+else:
+    print('DRAW')
