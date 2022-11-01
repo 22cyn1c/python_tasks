338a35553f292afc2a4fd3753c82bdaab098cd7e
renroy34 <115404396+renroy34@users.noreply.github.com>
2022-11-01 14:13:09 +0300

    Add files via upload

---
+++ acmpС/43.py
@@ -0,0 +1,13 @@
+k = input()
+n = len(k)
+z = 0
+t = 0
+for i in range(n):
+    a = i
+    if k[a] == '0':
+        z += 1
+        if t < z:
+            t = z
+    if k[a] == '1':
+        z = 0
+print(t)
