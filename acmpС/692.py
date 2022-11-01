a76ad439d535a9ca55c38dfea40f9a5d5012b213
renroy34 <115404396+renroy34@users.noreply.github.com>
2022-11-01 13:45:08 +0300

    Add files via upload

---
+++ acmpС/692.py
@@ -0,0 +1,16 @@
+a = int(input())
+n = 2
+b = 0
+t = 0
+if a == 0:
+    print('NO')
+elif a == 1:
+    print('YES')
+else:
+    while a > t:
+        b += 1
+        t = n**b
+    if a == t:
+        print('YES')
+    else:
+        print("NO")
