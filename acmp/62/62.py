49f8526266e52b7548759669642cd40a7173ca34
renroy34 <115404396+renroy34@users.noreply.github.com>
2022-11-14 12:57:08 +0300

    Add files via upload

---
+++ acmp/62/62.py
@@ -0,0 +1,11 @@
+a = input()
+if a[0] == 'A' or a[0] == 'C' or a[0] == 'E' or a[0] == 'G':
+    if a[1] == '1' or a[1] == '3' or a[1] == '5' or a[1] == '7':
+        print('BLACK')
+    else:
+        print('WHITE')
+if a[0] == 'B' or a[0] == 'D' or a[0] == 'F' or a[0] == 'H':
+    if a[1] == '2' or a[1] == '4' or a[1] == '6' or a[1] == '8':
+        print('BLACK')
+    else:
+        print('WHITE')
