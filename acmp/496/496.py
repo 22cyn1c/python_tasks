b7e38a69576d78a1904f62c4618c1626988dc0e1
renroy34 <115404396+renroy34@users.noreply.github.com>
2022-11-20 13:54:01 +0300

    Add files via upload

---
+++ acmp/496/496.py
@@ -0,0 +1,10 @@
+n = int(input())
+if n % 3 == 0:
+    print('GCV')
+elif n % 3 == 1:
+    print('VGC')
+elif n % 3 == 2:
+    print('CVG')
+    """
+    https://acmp.ru/index.asp?main=task&id_task=504
+    """
