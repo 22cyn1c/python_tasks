69de5b8f19a64037d8e7c353876f494f3b578bee
renroy34 <115404396+renroy34@users.noreply.github.com>
2023-03-22 13:16:17 +0300

    Add files via upload

--- acmp/53/53.py
+++ acmp/53/53.py
@@ -1,3 +1,6 @@
+"""
+https://acmp.ru/index.asp?main=task&id_task=53
+"""
 n, m = map(int, input().split())
 c = 0
 r = 0
@@ -5,28 +8,14 @@ g = 0
 b = 0
 for i in range(n):
     for j in range(m):
-        c += 1
-        if (i + 1) * (j + 1) % 2 == 0:
-            c -= 1
-            r += 1
-            if (i + 1) * (j + 1) % 3 == 0:
-                r -= 1
-                g += 1
-                if (i + 1) * (j + 1) % 5 == 0:
-                    g -= 1
-                    b += 1
-            elif (i + 1) * (j + 1) % 5 == 0:
-                r -= 1
-                b += 1
-        elif (i + 1) * (j + 1) % 3 == 0:
-            c -= 1
-            g += 1
-            if (i + 1) * (j + 1) % 5 == 0:
-                g -= 1
-                b += 1
-        elif (i + 1) * (j + 1) % 5 == 0:
-            c -= 1
+        if (i + 1) * (j + 1) % 5 == 0:
             b += 1
+        elif (i + 1) * (j + 1) % 3 == 0:
+            g += 1
+        elif (i + 1) * (j + 1) % 2 == 0:
+            r += 1
+        else:
+            c += 1
 print('RED :', r)
 print('GREEN :', g)
 print('BLUE :', b)
