44d645bf395b8f436ba6b78acb1d4f63a37f7b8c
renroy34 <115404396+renroy34@users.noreply.github.com>
2023-03-24 22:50:20 +0300

    Add files via upload

---
+++ acmp/1109/1109.py
@@ -0,0 +1,6 @@
+"""
+https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=26&id_problem=152
+"""
+a = int(input())
+print(f'The next number for the number {a} is {a + 1}.')
+print(f'The previous number for the number {a} is {a - 1}.')
