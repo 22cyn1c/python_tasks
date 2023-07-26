da2b72ff3961afbd2cb634a8815cf8aeac222201
renroy34 <115404396+renroy34@users.noreply.github.com>
2023-07-26 14:49:57 +0300

    Add files via upload

---
+++ acmp/1261/1261.py
@@ -0,0 +1,7 @@
+"""
+https://acmp.ru/asp/do/index.asp?main=topic&id_course=3&id_section=22&id_topic=127
+"""
+a1, b1 = map(int, input().split())
+a2, b2 = map(int, input().split())
+a3, b3 = map(int, input().split())
+print(b3 - b1, a2 - a1)
