48efdb54e60258e76e45374db5812565f1ba3679
renroy34 <115404396+renroy34@users.noreply.github.com>
2023-07-27 12:37:02 +0300

    Add files via upload

---
+++ acmp/1578/1578.py
@@ -0,0 +1,6 @@
+"""
+https://acmp.ru/asp/do/index.asp?main=task&id_course=3&id_section=22&id_topic=230&id_problem=1494
+"""
+a = int(input())
+b = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
+print(b[a])
